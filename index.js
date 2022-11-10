5b17ed779b754bbdf94a83e27956dd8398d3c6a2
ssobupak <andreubltzr11@gmail.com>
2022-11-10 16:41:59 +0100

    file changes

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-const posts = [
-  {
-    name: "Vincent van Gogh",
-    username: "vincey1853",
-    location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21,
-  },
-  {
-    name: "Gustave Courbet",
-    username: "gus1819",
-    location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4,
-  },
-  {
-    name: "Joseph Ducreux",
-    username: "jd1735",
-    location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment:
-      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152,
-  },
-];
-
-const postContainer = document.getElementById("posts-container");
+import { posts } from "./data.js";
 
 function displayPost() {
+  const postContainer = document.getElementById("posts-container");
+
   for (let i = 0; i < posts.length; i++) {
     postContainer.innerHTML += `
     <section class="posts">
